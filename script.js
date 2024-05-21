26a955a86ba7da9bbc08cf50cee7fb1314781468
ahmedSadoun <ahmedsaadoun866@gmail.com>
2024-05-21 10:10:37 +0300

    added refresh, fixed sunday bug, set some design

--- script.js
+++ script.js
@@ -17,23 +17,38 @@ async function fetchData() {
   res = await res.json();
   return res;
 }
+function compareDates(todaysDate) {
+  function stripTime(date) {
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  }
+  let datesResults = getCurrentWeekSunday();
+  let thisWeekSundayIs = datesResults.firstday;
+  let thisWeekThursdayIs = datesResults.lastday;
+  const sunday = stripTime(thisWeekSundayIs);
+  const thursday = stripTime(thisWeekThursdayIs);
+  const thisDayDate = stripTime(todaysDate);
 
-function calculateTotalTime(bookings) {
+  // Compare the dates
+  return thisDayDate <= thursday && thisDayDate >= sunday;
+}
+function calculateTotalTime(checkinDataList) {
   let totalHours = 0;
   let totalMinutes = 0;
-  let thisWeekSundayIs = getCurrentWeekSunday();
-  if (bookings && bookings.length <= 0) {
+
+  let workingDaysCount = 0;
+  if (checkinDataList && checkinDataList.length <= 0) {
     return;
   }
-  bookings?.forEach((booking) => {
+  checkinDataList?.forEach((checkinEntry) => {
     // Check if checkout is null
-    const dayDate = new Date(`${booking.date}T${booking.checkin}`);
-    if (dayDate >= thisWeekSundayIs) {
-      if (booking.checkout !== null && booking.checkin !== null) {
-        if (workingDaysList.includes(dayDate.getDay())) {
-          const checkinTime = new Date(`2000-01-01T${booking.checkin}`);
-          const checkoutTime = new Date(`2000-01-01T${booking.checkout}`);
+    const dayDate = new Date(`${checkinEntry.date}T${checkinEntry.checkin}`);
 
+    if (compareDates(dayDate)) {
+      if (checkinEntry.checkout !== null && checkinEntry.checkin !== null) {
+        if (workingDaysList.includes(dayDate.getDay())) {
+          const checkinTime = new Date(`2000-01-01T${checkinEntry.checkin}`);
+          const checkoutTime = new Date(`2000-01-01T${checkinEntry.checkout}`);
+          workingDaysCount += 1;
           const diffMilliseconds = checkoutTime - checkinTime;
           const diffMinutes = Math.floor(diffMilliseconds / (1000 * 60));
 
@@ -51,27 +66,33 @@ function calculateTotalTime(bookings) {
   totalHours += Math.floor(totalMinutes / 60);
   totalMinutes = totalMinutes % 60;
 
-  return { hours: totalHours, minutes: totalMinutes };
+  return {
+    hours: totalHours,
+    minutes: totalMinutes,
+    workingDaysCount: workingDaysCount,
+  };
 }
 
-fetchData().then((res) => {
-  // console.log(res.data);
-  let result = calculateTotalTime(res.data);
-  document.getElementById("totalHours").innerHTML =
-    result.hours + ":" + result.minutes;
-});
+function getAndBuild() {
+  fetchData().then((res) => {
+    // console.log(res.data);
+    let result = calculateTotalTime(res.data);
+    document.getElementById("totalHours").innerHTML =
+      "Total Working Hours: " + result.hours + ":" + result.minutes;
+    document.getElementById("workingDaysCount").innerHTML =
+      "Working Days Count: " + result.workingDaysCount;
+  });
+}
 
 function getCurrentWeekSunday() {
-  // Get the current date
-  const currentDate = new Date();
-  // Get the day of the week (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
-  const currentDayOfWeek = currentDate.getDay();
-  // Calculate the number of days to subtract to get to the previous Sunday
-  const daysToSubtract = currentDayOfWeek === 0 ? 7 : currentDayOfWeek;
-  // Calculate the date of the previous Sunday
-  const currentSunday = new Date(currentDate);
-  currentSunday.setDate(currentSunday.getDate() - daysToSubtract);
-  return currentSunday;
+  var curr = new Date(); // get current date
+  var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
+  var last = first + 6; // last day is the first day + 6
+
+  var firstday = new Date(curr.setDate(first));
+  var lastday = new Date(curr.setDate(last));
+
+  return { firstday: firstday, lastday: lastday };
 }
 
 function onLoginFunction() {
@@ -114,4 +135,18 @@ const myButton = document.getElementById("Submit");
 // Add a click event listener
 myButton.addEventListener("click", () => {
   onLoginFunction();
+});
+// Get the button by its ID
+const refresh = document.getElementById("Refresh");
+
+// Add a click event listener
+refresh.addEventListener("click", () => {
+  document.getElementById("totalHours").innerHTML = "";
+  document.getElementById("workingDaysCount").innerHTML = "";
+
+  getAndBuild();
+});
+
+window.addEventListener("load", async function () {
+  getAndBuild();
 });
